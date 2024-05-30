12f77e814cb9254dea5a9defe38bc5037ea73ff4
landmuc <landmucdev@gmail.com>
2024-05-30 16:56:58 +0200

    build.gradle compose compiler update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = libs.versions.kotlin.get() // 1.5.1
     }
     packaging {
         resources {
